0a9a44c7abe16f2b980760be5d3d3b3b7c91113f
CptTapo4ek <andreib@nextlogic.ro>
2024-04-29 22:58:12 +0300

    removed unused features

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:next_vision_flutter_app/src/biometric_verification/biometric_verification.dart';
 import 'package:next_vision_flutter_app/src/constants/size.dart';
-import 'package:next_vision_flutter_app/src/liveness_check/liveness_check.dart';
 
 
 List<CameraDescription> cameras = [];
@@ -41,25 +40,14 @@ class Home extends StatelessWidget {
               child: Column(
                 children: [
                   TextButton(
-                    onPressed: () => Get.to(const LivenessCheck(isTest: true)),
-                    child: Text(
-                      "Liveness Check V1",
-                      style: TextStyle(
-                        fontSize: 18
-                      ),
-                    )
-                  ),
-                  SizedBox(height: 30),
-                  TextButton(
                     onPressed: () => Get.to(const BiometricVerification()),
                     child: Text(
-                      "Liveness Check V2",
+                      "Liveness Check",
                       style: TextStyle(
                         fontSize: 18
                       ),
                     )
                   ),
-                  SizedBox(height: 30),
                 ],
               ),
             ),
